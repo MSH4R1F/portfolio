818d1f75be33508b3ba538698468ef2f7a8eeb80
Mohamed Sharif <mas222@ic.ac.uk>
2023-05-28 17:00:29 +0100

    Updates

---
+++ precache-manifest.ff9e785221b5cfc2908ee55cd3c74014.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "17dd1c38c9be0087efd8",
+    "url": "/static/css/main.1fef631e.chunk.css"
+  },
+  {
+    "revision": "17dd1c38c9be0087efd8",
+    "url": "/static/js/main.d29f1c8e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "53392adf9c6780b79ac3",
+    "url": "/static/js/2.54755705.chunk.js"
+  },
+  {
+    "revision": "2110dab931bb43115c757cb494c1499b",
+    "url": "/static/media/BlogGraphic-Blockchains-01-1.2110dab9.png"
+  },
+  {
+    "revision": "5dcab01f7349078644fc741d63715423",
+    "url": "/static/media/cv.5dcab01f.pdf"
+  },
+  {
+    "revision": "7f6e5fbf244c85c34acd88e1f52ff7f7",
+    "url": "/static/media/1657388881390 - Copy.7f6e5fbf.jpg"
+  },
+  {
+    "revision": "18a0657d5c1e7d29fff72bd0dbcec331",
+    "url": "/index.html"
+  }
+];
